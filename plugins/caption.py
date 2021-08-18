92aa85c00472c4d9c6f72c2436b34b8f6250d243
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-18 15:48:05 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,11 +5,21 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 
-CAPTION=Config.CAPTION
-
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     media = message.audio or message.video or message.document or message.animation
     if (media is not None) and (media.file_name is not None):
-        file = media.file_name
-    await message.edit(CAPTION)
+        m = media.file_name
+        epi = m.split(None
+        ep = "E1" or "E2" or "E3" or "E4" or "E5" or "E6" or "E7" or "E8" or "E9"
+        if '720P' in m:
+            y = '720'
+        if '480P' in m:
+            y = '480'
+        if '1080P' in m:
+            y = '1080'
+        if '240P' in m:
+            y = '240'
+
+
+    await message.edit(
