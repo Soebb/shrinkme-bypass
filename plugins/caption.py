96608c18238010620e6a6d770cad51f011470840
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-18 16:13:20 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -10,7 +10,7 @@ async def caption(client, message: Message):
     media = message.audio or message.video or message.document or message.animation
     if (media is not None) and (media.file_name is not None):
         m = media.file_name
-        ep = "E1" or "E2" or "E3" or "E4" or "E5" or "E6" or "E7" or "E8" or "E9"
+        ep = "E0" or "E1" or "E2" or "E3" or "E4" or "E5" or "E6" or "E7" or "E8" or "E9"
         if ep in m.split()[3]:
             E = m.split()[3]
         if ep in m.split()[4]:
@@ -34,6 +34,8 @@ async def caption(client, message: Message):
             Q = '1080'
         if '240P' in m:
             Q = '240'
+        N = m.replace("@dlmacvin2 -", " ")
+        n = N.split(f"{E}")[0]
 
 
     await message.edit(
