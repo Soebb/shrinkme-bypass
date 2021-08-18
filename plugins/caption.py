7c9c85b19b5da66cc1ccdb746cdba571b2162c0d
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-18 16:34:37 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -11,21 +11,6 @@ async def caption(client, message: Message):
     if (media is not None) and (media.file_name is not None):
         m = media.file_name
         ep = "E0" or "E1" or "E2" or "E3" or "E4" or "E5" or "E6" or "E7" or "E8" or "E9"
-        if ep in m.split()[3]:
-            E = m.split()[3]
-        if ep in m.split()[4]:
-            E = m.split()[4]
-        if ep in m.split()[5]:
-            E = m.split()[5]
-        if ep in m.split()[6]:
-            E = m.split()[6]
-        if ep in m.split()[7]:
-            E = m.split()[7]
-        if ep in m.split()[8]:
-            E = m.split()[8]
-        if ep in m.split()[9]:
-            E = m.split()[9]
-        e = E.replace("E", " ")
         if '720P' in m:
             Q = '720'
         if '480P' in m:
@@ -35,5 +20,24 @@ async def caption(client, message: Message):
         if '240P' in m:
             Q = '240'
         N = m.replace("@dlmacvin2 -", " ")
+        if ep in N.split()[1]:
+            E = N.split()[1]
+        if ep in N.split()[2]:
+            E = N.split()[2]
+        if ep in N.split()[3]:
+            E = N.split()[3]
+        if ep in N.split()[4]:
+            E = N.split()[4]
+        if ep in N.split()[5]:
+            E = N.split()[5]
+        if ep in N.split()[6]:
+            E = N.split()[6]
+        if ep in N.split()[7]:
+            E = N.split()[7]
+        if ep in N.split()[8]:
+            E = N.split()[8]
+        if ep in N.split()[9]:
+            E = N.split()[9]
+        e = E.replace("E", " ")
         n = N.split(f"{E}")[0]
         await message.edit(
