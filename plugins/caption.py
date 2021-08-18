0c8149f77806873712dd603eb12e089430863231
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-18 16:17:31 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -7,7 +7,7 @@ from pyrogram.errors import FloodWait
 
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
-    media = message.audio or message.video or message.document or message.animation
+    media = message.video or message.document
     if (media is not None) and (media.file_name is not None):
         m = media.file_name
         ep = "E0" or "E1" or "E2" or "E3" or "E4" or "E5" or "E6" or "E7" or "E8" or "E9"
@@ -36,6 +36,4 @@ async def caption(client, message: Message):
             Q = '240'
         N = m.replace("@dlmacvin2 -", " ")
         n = N.split(f"{E}")[0]
-
-
-    await message.edit(
+        await message.edit(
