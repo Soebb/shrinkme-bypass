b69cbf019b358093cf2d1f6527a5c5ba691ed801
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-18 17:40:29 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -40,11 +40,35 @@ async def caption(client, message: Message):
             E = N.split()[9]
         e = E.replace("E", " ")
         n = N.split(f"{E}")[0]
+        y = N.replace("720P", " ")
+        Yr = "20" or "19"
+        if Yr in y.split()[1]
+            Y = y.split()[1]
+        if Yr in y.split()[2]
+            Y = y.split()[2]
+        if Yr in y.split()[3]
+            Y = y.split()[3]
+        if Yr in y.split()[4]
+            Y = y.split()[4]
+        if Yr in y.split()[5]
+            Y = y.split()[5]
+        if Yr in y.split()[6]
+            Y = y.split()[6]
+        if Yr in y.split()[7]
+            Y = y.split()[7]
+        if Yr in y.split()[8]
+            Y = y.split()[8]
+        if Yr in y.split()[9]
+            Y = y.split()[9]
+        if Y:
+            YR = f"\n👌سال: {Y}"
+        else:
+            YR = f"\n👌سال:"
         if Q:
             q = f"\n🔷کیفیت: {Q}"
         if E:
             await message.edit(f"♨️سریال: {n} \n👌قسمت: {e} {q} \n🔻تماشای آنلاین بدون فیلتر شکن: \n🆔👉 @dlmacvin_new")
         else:
-            await message.edit(f"♨️سریال: {n} \n👌قسمت: {e} {q} \n🔻تماشای آنلاین بدون فیلتر شکن: \n🆔👉 @dlmacvin_new")
+            await message.edit(f"♨️فیلم: {n} {YR} {q} \n🔻تماشای آنلاین بدون فیلتر شکن: \n🆔👉 @dlmacvin_new")
         
    
