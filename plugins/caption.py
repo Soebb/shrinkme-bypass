29375661fb078e5764c20d48723f469868a42373
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-19 00:53:57 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -87,6 +87,7 @@ async def caption(client, message: Message):
                 YR = f"\n👌سال: {Y}"
             W = "20" or "19"
             if not W in D:
+                Y = " "
                 P = m.split("0P")[0]
                 f = P.replace("72", " ").replace("48", " ").replace("108", " ").replace("24", " ")
                 YR = f"\n👌سال:"
