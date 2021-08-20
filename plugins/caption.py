be5f21c913b90169cf40cc072024b154d270b6cb
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-21 01:33:22 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -6,8 +6,8 @@ from pyrogram.errors import FloodWait
 async def caption(client, message: Message):
     media = message.video or message.document
     if (media is not None) and (media.file_name is not None):
-        m = media.file_name.replace("Fragmanı", " ").replace("Fragmanlarım", " ").replace("ı", "i").replace("İ", "I").replace("ö", "o").replace("Ö", "O").replace("Ü", "U").replace("ü", "u").replace("ë", "e").replace("Ë", "E").replace("Ä", "A").replace("ç", "c").replace("Ç", "C").replace("ş", "s").replace("Ş", "S").replace("ğ", "g").replace("Ğ", "G").replace("ä", "a")
-        D = m.replace("720P", " ").replace("E20", " ").replace("E120", " ").replace("E220", " ").replace("E320", " ")
+        m = media.file_name.replace("Fragmanı", "").replace("Fragmanlarım", "").replace("ı", "i").replace("İ", "I").replace("ö", "o").replace("Ö", "O").replace("Ü", "U").replace("ü", "u").replace("ë", "e").replace("Ë", "E").replace("Ä", "A").replace("ç", "c").replace("Ç", "C").replace("ş", "s").replace("Ş", "S").replace("ğ", "g").replace("Ğ", "G").replace("ä", "a")
+        D = m.replace("720P", "").replace("E20", "").replace("E120", "").replace("E220", "").replace("E320", "")
         N = m.replace("@dlmacvin2 -", "").replace("@dlmacvin -", "")
         Z = media.file_name
         fa = " "
@@ -41,7 +41,7 @@ async def caption(client, message: Message):
                 tz += "#ششم"
             if X.__contains__("a") or X.__contains__("o") or X.__contains__("i") or X.__contains__("c") or X.__contains__("b") or X.__contains__("e") or X.__contains__("l") or X.__contains__("n") or X.__contains__("m"):
                 Yd = X.replace(" ", "_")
-                Lo += f"#{Yd}"
+                Lo += "#" + f"{Yd}"
                 V = Kh.split("Bolum")[0]
                 E = V.replace(".", "")
             else:
