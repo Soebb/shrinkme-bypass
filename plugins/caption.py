776e4feeac13e270300c1f31e22d8c8783403eba
Soebb <87156166+Soebb@users.noreply.github.com>
2021-10-19 18:47:58 +0330

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 async def main(bot, m):
     if not os.path.isdir('temp/'):
         os.makedirs('temp/')
-    await m.download('temp/v.mp4')
-    os.system("ffmpeg -i temp/v.mp4 -r 10 -f image2 output_%05d.jpg")
+    await m.download('plugins/v.mp4')
+    os.system("ffmpeg -i plugins/v.mp4 -r 10 -f image2 temp/output_%05d.jpg")
     for file in tqdm(sort_alphanumeric(os.listdir("temp"))):
         await m.reply_photo(photo=f"temp/{file}")
