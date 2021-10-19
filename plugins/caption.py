e7c078ee0171e32337dee828673db21f2b6c294b
Soebb <87156166+Soebb@users.noreply.github.com>
2021-10-19 11:35:58 +0330

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,6 +5,6 @@ import os
 
 @Client.on_message(filters.private & filters.video)
 async def main(bot, m):
-    await m.download('v.mp4')
-    os.system("ffmpeg -ss 15 -i v.mp4 -vframes 1 -q:v 2 output.jpg")
-    await m.reply_photo(photo='output.jpg')
+    await m.download('plugins/v.mp4')
+    os.system("ffmpeg -ss 15 -i plugins/v.mp4 -vframes 1 -q:v 2 plugins/output.jpg")
+    await m.reply_photo(photo='plugins/output.jpg')
