41cfee48538982e389e58d90a9bca190bdd08d1b
Soebb <87156166+Soebb@users.noreply.github.com>
2024-02-11 15:41:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from pyromod import listen
-import requests
-import yt_dlp
+from bs4 import BeautifulSoup
+import cloudscraper
 import os, time
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-'''
-files_path = [os.path.abspath(x) for x in os.listdir("app")]
-print(files_path)
-'''
+
+
 if "BOT_TOKEN" in os.environ:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = os.environ.get("API_ID")
@@ -26,14 +23,12 @@ Bot = Client(
 
 START_TXT = """
 Hi {}
-I am subtitle extractor Bot.
-> `I can extract hard-coded subtitle from videos.`
-Send me a video to get started.
+I am test Bot.
 """
 
 START_BTN = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton("Source Code", url="https://github.com/samadii/VidSubExtract-Bot"),
+        InlineKeyboardButton("Source Code", url="https://github.com/samadii/"),
         ]]
     )
 
@@ -49,14 +44,29 @@ async def start(bot, update):
     )
 
 
-@Bot.on_message(filters.private & filters.video)
-async def main(bot, m):
-    await m.download("temp/v1.mp4")
-    merge = await bot.ask(m.chat.id,'ویدیویی که میخوای ادغام شه؟بفرس', filters=filters.video)
-    await bot.download_media(message=merge.video, file_name="temp/v2.mp4")
-    os.system("/app/mkvmerge.exe -o temp/v3.mp4 temp/v1.mp4 +temp/v2.mp4")
-    time.sleep(10)
-    await m.reply_video(video="temp/v3.mp4")
+@Bot.on_message(filters.private & filters.text)
+async def main(_, m):
+    result = shrinkme(m.text)
+    await m.reply(result)
+
+def shrinkme(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    DOMAIN = "https://en.shrinke.me/"
+    url = url[:-1] if url[-1] == '/' else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://mrproblogger.com/"
+    h = {"referer": ref}
+    resp = client.get(final_url,headers=h)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input")
+    data = { input.get('name'): input.get('value') for input in inputs }
+    #print(data)
+    h = { "x-requested-with": "XMLHttpRequest" }
+    time.sleep(15)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try: return r.json()['url']
+    except: return "Something went wrong :("
 
 
 Bot.run()
