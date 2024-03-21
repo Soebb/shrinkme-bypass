afa487234a3d7790db5299fdf08412e3f2515a32
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-21 12:19:29 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,55 +1,12 @@
 from bs4 import BeautifulSoup
 import cloudscraper
 import os, time
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+TOKEN = os.environ.get('TOKEN')
 
-if "BOT_TOKEN" in os.environ:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-else:
-    BOT_TOKEN = " "
-    API_ID = " "
-    API_HASH = " "
-
-Bot = Client(
-    "Bot",
-    bot_token = BOT_TOKEN,
-    api_id = API_ID,
-    api_hash = API_HASH
-)
-
-START_TXT = """
-Hi {}
-I am test Bot.
-"""
-
-START_BTN = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton("Source Code", url="https://github.com/samadii/"),
-        ]]
-    )
-
-
-@Bot.on_message(filters.command(["start"]))
-async def start(bot, update):
-    text = START_TXT.format(update.from_user.mention)
-    reply_markup = START_BTN
-    await update.reply_text(
-        text=text,
-        disable_web_page_preview=True,
-        reply_markup=reply_markup
-    )
-
-
-@Bot.on_message(filters.private & filters.text)
-async def main(_, m):
-    result = shrinkme(m.text)
-    await m.reply(result)
-
-def shrinkme(url):
+def shrinkme(update, context):
+    url = update.message['text']
     client = cloudscraper.create_scraper(allow_brotli=False)
     DOMAIN = "https://en.shrinke.me/"
     url = url[:-1] if url[-1] == '/' else url
@@ -65,8 +22,18 @@ def shrinkme(url):
     h = { "x-requested-with": "XMLHttpRequest" }
     time.sleep(15)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try: return r.json()['url']
-    except: return "Something went wrong :("
+    try: return update.message.reply_text(r.json()['url'])
+    except: return update.message.reply_text("Something went wrong")
 
 
-Bot.run()
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text("Hi, I am shrinkme url bypass bot")
+
+
+if __name__=='__main__':
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(MessageHandler(Filters.text, shrinkme))
+    dispatcher.add_handler(CommandHandler("start", start))
+    updater.start_polling()
